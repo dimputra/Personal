989e9d286cf6ce808fdb6119a2dfd631e5f6bf49
dimputra <119169951+dimputra@users.noreply.github.com>
2024-12-22 18:56:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY MOTHER'S DAY').split('')
+    const titles = ('HAPPY MOTHERS DAY').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
